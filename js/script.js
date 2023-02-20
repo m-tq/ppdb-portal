9bc19c0ba118cc5c25d86024d40fc17f6b9a3a87
k4ng3s <payment.adsku@gmail.com>
2023-02-20 15:18:20 +0700

    update

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//All main JS Script
+(() => {
+
+  'use strict'
+
+  var toggleTheme = document.getElementById('toggleTheme')
+  var imgl = document.getElementById('svgregis') 
+
+  toggleTheme.addEventListener('click', () => {
+    if (toggleTheme.classList.contains('i-dark')){
+      imgl.src = "/img/regisd.svg"
+      toggleTheme.classList.add('i-light')
+      toggleTheme.classList.remove('i-dark')      
+      document.documentElement.setAttribute('data-bs-theme', 'dark')
+    }else{
+      imgl.src = "/img/regis.svg"
+      toggleTheme.classList.add('i-dark')
+      toggleTheme.classList.remove('i-light')      
+      document.documentElement.setAttribute('data-bs-theme', 'light')
+    }
+})
+
+})()
+  
