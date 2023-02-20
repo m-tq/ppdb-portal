e24ccff45db70c5d876bd1bd08cdb80b3cb20264
taufik <payment.adsku@gmail.com>
2023-02-20 15:31:32 +0700

    update

--- js/script.js
+++ js/script.js
@@ -8,12 +8,12 @@
 
   toggleTheme.addEventListener('click', () => {
     if (toggleTheme.classList.contains('i-dark')){
-      imgl.src = "/img/regisd.svg"
+      imgl.src = "img/regisd.svg"
       toggleTheme.classList.add('i-light')
       toggleTheme.classList.remove('i-dark')      
       document.documentElement.setAttribute('data-bs-theme', 'dark')
     }else{
-      imgl.src = "/img/regis.svg"
+      imgl.src = "img/regis.svg"
       toggleTheme.classList.add('i-dark')
       toggleTheme.classList.remove('i-light')      
       document.documentElement.setAttribute('data-bs-theme', 'light')
